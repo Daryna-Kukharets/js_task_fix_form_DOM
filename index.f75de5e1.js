93d378eccdb5776ae81072202488d9206e109453
Daryna Kukharets <darinakuharec61@gmail.com>
2024-08-03 22:56:23 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll("input")
+];
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    const labelName = input.name;
+    const nameReplace = labelName.replace(/([A-Z])/g, " $1");
+    const firstLetter = nameReplace[0].toUpperCase();
+    const formatName = firstLetter + nameReplace.slice(1);
+    label.className = "field-label";
+    label.htmlFor = input.id;
+    label.textContent = formatName;
+    input.insertAdjacentElement("beforebegin", label);
+    input.placeholder = formatName;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
